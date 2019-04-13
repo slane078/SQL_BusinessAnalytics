0d91f5ebe845365c72d4d99b483d547eb9948ba0
Sean Lane <slane078@gmail.com>
2019-04-13 10:09:17 -0700

    update

--- web/static/js/config.js
+++ web/static/js/config.js
@@ -1,8 +1,8 @@
-
+// funtion for loading dashboard from web
 function initViz() {
+    // variable for our container on HTML
   var placeholderDiv = document.getElementById("vizContainer");
+    // URL of dashboard created with data
   var url = "https://public.tableau.com/views/project2_dashboard/Dashboard1?:embed=y&:display_count=yes&publish=yes"
  var viz = new tableau.Viz(placeholderDiv, url);  
 }
-
-// Create the viz after the page is done loading
